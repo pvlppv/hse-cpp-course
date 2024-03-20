974eb584aeca269891dcf781f86671b82cc69d1a
Pavel Popov <paulpopov78@gmail.com>
2024-03-20 10:15:33 +0300

    Modify contest04

--- contests/contest04/solution.h
+++ contests/contest04/solution.h
@@ -1,3 +1,4 @@
+
 #include <algorithm>
 #include <iostream>
 #include <map>
@@ -14,12 +15,12 @@ struct Task {
 
     Task(const std::string& text, const std::string& date, const std::map<std::string, int>& workloads);
     int getTotalLoad() const;
+    bool operator<(const Task& other) const;
 };
 
 // task 1
-Task::Task(const std::string& text, const std::string& date, const std::map<std::string, int>& workloads)
+Task::Task(const std::string& text, const std::string& date, const std::map<std::string, int>& workloads) 
     : text(text), date(date), workloads(workloads) {}
-
 
 struct Worker {
     std::string name;
@@ -33,95 +34,120 @@ struct Worker {
 };
 
 // task 1
-Worker::Worker(const std::string& name, const std::string& login, int maxLoad) 
+Worker::Worker(const std::string& name, const std::string& login, int maxLoad)
     : name(name), login(login), maxLoad(maxLoad) {}
-
 
 // task 2
 int Task::getTotalLoad() const
 {
     int total = 0;
-    for (const auto& i : workloads) {
-        total += i.second;
+    for (const auto& workload : workloads)
+    {
+       total += workload.second;
     }
     return total;
 }
 
-// task 3
-bool compareTasks(const Task& task1, const Task& task2) {
-    if (task1.getTotalLoad() != task2.getTotalLoad()) {
+// Task operator <
+bool Task::operator<(const Task& other) const
+{
+    if (this->getTotalLoad() != other.getTotalLoad())
+    {
+        return this->getTotalLoad() < other.getTotalLoad();
+    }
+    if (this->date != other.date)
+    {
+        return this->date < other.date;
+    }
+    return this->text < other.text;
+}
+
+// Comparator for std::sort
+bool compareTasks(const Task& task1, const Task& task2)
+{
+    if (task1.getTotalLoad() != task2.getTotalLoad())
+    {
         return task1.getTotalLoad() < task2.getTotalLoad();
     }
-
-    if (task1.date != task2.date) {
+    if (task1.date != task2.date)
+    {
         return task1.date < task2.date;
     }
-
     return task1.text < task2.text;
 }
 
+// task 3
 void sortTasks(std::vector<Task>& tasks)
 {
-    std::sort(tasks.begin(), tasks.end(), compareTasks);
+    std::sort(tasks.begin(), tasks.end());
 }
 
 // task 4
-int getWorkerLoad(const std::vector<Task>& allTasks, const Worker& worker) {
-    int count = 0;
-    for (const auto& i : allTasks) {
-        for (const auto& j: i.workloads) {
-            if (j.first == worker.login) {
-                count += j.second;
+int getWorkerLoad(const std::vector<Task>& allTasks, const Worker& worker)
+{
+    int total = 0;
+    for (const auto& task : allTasks)
+    {
+        for (const auto& workload : task.workloads)
+        {
+            if (workload.first == worker.login)
+            {
+                total += workload.second;
             }
-        }        
+        }
     }
-    return count;
+    return total;
 }
 
 // task 5
 void addTask(std::vector<Task>& tasks, const Task& newTask, const std::vector<Worker>& workers)
 {
-    for (const auto& i : newTask.workloads) {
-        for (const auto& j : workers) {
-            if (i.first == j.login) {
-                int totalLoad = getWorkerLoad(tasks, j) + i.second;
-                if (totalLoad > j.maxLoad) {
+    for (const auto& newtask : newTask.workloads)
+    {
+        for (const auto& worker : workers)
+        {
+            if (newtask.first == worker.login)
+            {
+                if (getWorkerLoad(tasks, worker) + newtask.second > worker.maxLoad)
+                {
                     throw std::runtime_error("Overworked");
-                }
+                } 
             }
         }
     }
     tasks.push_back(newTask);
 }
 
-// task 6
-bool compareWorkers(std::pair<std::string, int> worker1, std::pair<std::string, int> worker2) {
+bool compareWorkers(const std::pair<std::string, int> worker1, const std::pair<std::string, int> worker2)
+{
     return worker1.first < worker2.first;
 }
 
+// task 6
 void addTaskVerbose(std::vector<Task>& tasks, const Task& newTask, const std::vector<Worker>& workers)
 {
-    tasks.push_back(newTask);
-    std::pair<std::string, int> pair;
-    std::vector<std::pair<std::string, int>> overworked_workers;
+    std::vector<std::pair<std::string, int>> overworked;
 
-    for (const auto& i : workers) {
-        int totalLoad = getWorkerLoad(tasks, i);
-        pair.first = i.login;
-        pair.second = totalLoad - i.maxLoad;
-        if (totalLoad > i.maxLoad) {
-            overworked_workers.push_back(pair);
+    tasks.push_back(newTask);
+
+    for (const auto& worker : workers)
+    {
+        int worker_load = getWorkerLoad(tasks, worker);
+        if (worker_load > worker.maxLoad)
+        {
+            overworked.push_back({worker.login, worker_load - worker.maxLoad});
         }
     }
 
-    if (!overworked_workers.empty() > 0) {
-        std::sort(overworked_workers.begin(), overworked_workers.end(), compareWorkers);
+    if (!overworked.empty())
+    {
+        std::sort(overworked.begin(), overworked.end(), compareWorkers);
         std::string msg = "Overworked: ";
-        for (int i = 0; i < overworked_workers.size(); i++) {
-            msg += overworked_workers[i].first + " by " + std::to_string(overworked_workers[i].second) + ", ";
+        for (const auto& overworker : overworked)
+        {
+            msg += overworker.first + " by " + std::to_string(overworker.second) + ", ";
         }
-        msg.erase(msg.end() - 2, msg.end());
+        msg.erase(msg.rfind(","));
         throw std::runtime_error(msg);
     }
 }
-
