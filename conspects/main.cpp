295eae0605365632f40b53d0162311c659b5c816
Pavel Popov <paulpopov78@gmail.com>
2024-04-23 20:45:36 +0300

    Update conspects

--- conspects/main.cpp
+++ conspects/main.cpp
@@ -1,10 +1,32 @@
-#include "iostream"
-#include "sstream"
-#include "fstream"
-
 // sstream
 std::string line;
 std::getline(fileStream, line);
 std::stringstream ss(line);
 std::string field;
 std::getline(ss, field, ';');
+
+
+// custom comparator
+bool comparePeople(const Person& a, const Person& b)
+{
+    return a.age < b.age;
+}
+std::sort(people.begin(), people.end(), comparePeople);
+
+
+// file i/o
+std::ifstream inputFile("input.txt");
+std::string line;
+while (std::getline(inputFile, line)) {
+    cout << line << endl;
+}
+char c;
+while (infile.get(c)) {
+    cout << c;
+}
+inputFile.close();
+
+std::ofstream outputFile("output.txt");
+outputFile << "Hello, world!" << endl;
+outputFile.close();
+
